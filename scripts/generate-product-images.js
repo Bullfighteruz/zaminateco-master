edec29aff4d28b2bfc9997ab05d5be758e5ee64d
SyraXuz <trustautogeeks3@gmail.com>
2026-08-10 11:18:01 -0400

    fix(assets): update mockData, iconMatcher, and productImages paths to match converted AVIF files

--- scripts/generate-product-images.js
+++ scripts/generate-product-images.js
@@ -52,7 +52,7 @@ function getImageFiles(dirPath) {
     }
     
     const files = fs.readdirSync(dirPath);
-    const imageExtensions = ['.webp'];
+    const imageExtensions = ['.avif', '.webp', '.jpg', '.png'];
     
     return files
       .filter(file => {
